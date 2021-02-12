6a3f0b86072f4b055595196c6d3bf128ad6a1e79
정철희 <jch@jeongcheolhuiui-MacBookPro.local>
2021-02-12 22:57:35 +0900

    change chap19-6

--- chap19/chap19-6.py
+++ chap19/chap19-6.py
@@ -4,17 +4,25 @@
 # 특정 값이 되는 부분 수열의 개수를 반환.
 
 # 임의의 수 init을 받아
-# 특정 규칙에 따라 난수를 발생시키는 함수 RNG
-def RNG(init):
-    init = (((init * 214013) + 2531011) % pow(2,32)) 
-    return (init % 10000 + 1)
+# 특정 규칙에 따라 난수를 발생시키는 클래스 RNG
+# seed 값과 결과로 나온 랜덤 값은 별개이므로
+# 이를 구분하기 위해 class형태로 만들어 class변수에
+# seed 값을 따로 저장함. 초반에 좀 헤맴.
+class RNG(object):
+    def __init__(self, seed):
+        self.seed = seed
+    def renew(self):
+        self.seed = (((self.seed * 214013) + 2531011) % pow(2,32)) 
+    def getvalue(self):
+        return (self.seed % 10000 + 1)
 
 # 함수명은 countRanges.
 # 입력은 일정 수열의 길이, 부분 수열의 특정 합.
 # 출력은 입력 수열의 부분 수열의 합 중 특정 값을 만족하는 부분 수열의 수.
 def countRanges(n, k):
     # 입력으로 들어갈 수열, 부분 수열 합을 1983으로 초기화. 
-    newSignal = 1983
+    seed = RNG(1983)
+    newSignal = seed.getvalue()
     rangeSignal = [newSignal]
     ret = 0
     rangeSum = newSignal
@@ -22,7 +30,8 @@ def countRanges(n, k):
     for _ in range(n):
         # 한 번 구간을 돌 때마다 임의의 난수를 생성하고
         # 구간에 값을 추가 및 부분 수열 합에 더해줌.
-        newSignal = RNG(newSignal)
+        seed.renew() 
+        newSignal = seed.getvalue()
         rangeSum = rangeSum + newSignal
         rangeSignal.append(newSignal)
 
