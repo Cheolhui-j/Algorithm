1c1ad684f734835b2dba1406e9a7c73234bd7ee4
정철희 <jch@jeongcheolhuiui-MacBookPro.local>
2021-03-05 23:45:47 +0900

    fix : modify chap21-4

--- chap21/chap21-4.py
+++ chap21/chap21-4.py
@@ -1,4 +1,5 @@
-class Tree(object):
+
+class Binery_Tree(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -7,20 +8,36 @@ class Tree(object):
         self.size = 1
     
     def setleft(self, node):
-        assert isinstance(node, Tree)
         self.left = node
-        self.calcsize()
-
-    def setright(self, node):
-        assert isinstance(node, Tree)
-        self.right = node
-
-    def calcsize():
         if self.left is not None:
             self.size = self.size + self.left.size
+
+    def setright(self, node):
+        self.right = node
         if self.right is not None:
             self.size = self.size + self.right.size
 
-def isDominated(x, y):
+def getTree(x, y):
 
+    if x is None or y is None:
+        return None
 
+    tree = Binery_Tree(x[0], y[0])
+
+    return tree
+        
+x = [72, 57, 74, 64]
+y = [50, 67, 55, 60]
+
+tree = getTree(x, y)
+
+def isDominated(x, y, node):
+
+    if node.x < x and node.y < y:
+        return 'Dominate'
+
+    elif node.x > x and node.y > y:
+        return 'Dominated'
+        
+    else :
+        return 'merge'
